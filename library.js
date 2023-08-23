d2b33e86c99d479ed89e362fcd93c44900004617
DidasMba <daniel.mbarushi@gmail.com>
2023-08-23 15:47:38 +0200

    creating book object

---
+++ library.js
@@ -0,0 +1,11 @@
+const myLibrary = [];
+//constructor function for creating book object 
+function Book(){
+ this.title = title;
+ this.autor = autor;
+ this.pages = pages;
+}
+
+function addBookToLibrary() {
+    // do stuff here
+  }
