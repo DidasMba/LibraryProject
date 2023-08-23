d55c4868fa30fb384725b44f9ae4562134a39f99
DidasMba <daniel.mbarushi@gmail.com>
2023-08-23 16:06:55 +0200

    example usage

--- library.js
+++ library.js
@@ -1,11 +1,12 @@
 const myLibrary = [];
 //constructor function for creating book object 
-function Book(){
+function Book(title, autor, pages){
  this.title = title;
  this.autor = autor;
  this.pages = pages;
 }
-
-function addBookToLibrary() {
-    // do stuff here
+// function to add a new book to the library array
+function addBookToLibrary(title, autor, pages) {
+    const newBook = new Book(title, autor, pages);
+    myLibrary.push(newBook);
   }
