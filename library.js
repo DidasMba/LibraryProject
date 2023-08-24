8da3bc43310b63d385cc01338a48f288f6c2216c
DidasMba <daniel.mbarushi@gmail.com>
2023-08-24 15:42:29 +0200

    display books

--- library.js
+++ library.js
@@ -1,10 +1,10 @@
 const myLibrary = [];
 //constructor function for creating book object 
-function Book(title, autor, pages){
- this.title = title;
- this.autor = autor;
- this.pages = pages;
-}
+function Book(title, author, pages) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+  }
 // function to add a new book to the library array
 function addBookToLibrary(title, autor, pages) {
     const newBook = new Book(title, autor, pages);
@@ -15,7 +15,7 @@ function addBookToLibrary(title, autor, pages) {
   addBookToLibrary("The Hobbit", "J.R.R. Tolkien", 300);
   addBookToLibrary("To Kill a Mockingbird", "Harper Lee", 320);
 
-  console.log(myLibrary);
-  
+  console.log(myLibrary); // This will show the array of book objects
+
 
 
